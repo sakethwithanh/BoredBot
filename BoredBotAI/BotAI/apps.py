ed308f185f56d89affd99e03ba7fe7872049e276
sakethwithanh <saketh21092@iiitd.ac.in>
2023-11-18 18:41:05 +0530

    base page

---
+++ BoredBotAI/BotAI/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BotaiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'BotAI'
---
+++ BoredBotAI/BotAI/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+
+# Create your views here.
+
+def landPage(request):
+    return  render(request, 'land.html')
+def homePage(request):
+    pass
+ 
